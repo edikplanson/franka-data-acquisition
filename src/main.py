e5d1e138f0fadef10d867a87ba29a0e1e6855741
edikplaas <edik.planson@laas.fr>
2026-04-29 09:54:33 +0200

    Added camera reader

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import time
 from tactile_reader import TactileSensor
 
 if __name__ == "__main__":
-    sensor = TactileSensor("/dev/ttyUSB0", 2_000_000, 12, 32, 20, 60, 30, 0.2)
+    sensor = TactileSensor("/dev/ttyUSB0", baud=2_000_000, rows=12, cols=32, thresh=20, noise_scale=60, init_frames=30, alpha=0.2)
     while True:
         sensor.update()
         sensor.visualizer() # visualize the current contact data
-        print(sensor.readSensor()[1]) # print timestamp of the latest frame
-        time.sleep(0.01)
+        data, timestamp = sensor.readSensor() # read the latest contact data and timestamp
+        print(timestamp) 
+        time.sleep(0.04) # 25 Hz update rate
